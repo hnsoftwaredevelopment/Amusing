58aed85270b7a54254d64863e5aaee4378c050bc
Herbert Nijkamp <herbert.nijkamp@voortman.net>
2025-08-29 16:41:56 +0200

    Completed User maintenance, perhaps in the future a better logo fr saving the password, but for now it works as it is.

--- Amusing/Models/UserModel.cs
+++ Amusing/Models/UserModel.cs
@@ -1,27 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿namespace Amusing.Models;
 
-namespace Amusing.Models;
-
-public partial class UserModel : IValidatableObject
+public partial class UserModel
 {
     public uint UserId { get; set; }
-
-    [Required( ErrorMessage = "Gebruikersnaam mag niet leeg zijn" )]
     public string Username { get; set; } = null!;
-
     public string Password { get; set; } = null!;
-
-    [Required( ErrorMessage = "Rol mag niet leeg zijn" )]
     public string Role { get; set; } = null!;
-
     public string LastLoginDate { get; set; } = "nooit";
-    public IEnumerable<ValidationResult> Validate( ValidationContext validationContext )
-    {
-        if ( UserId == 0 && string.IsNullOrWhiteSpace( Password ) )
-        {
-            yield return new ValidationResult(
-                "Wachtwoord mag niet leeg zijn voor een nieuwe gebruiker",
-                new [ ] { nameof( Password ) } );
-        }
-    }
 }
