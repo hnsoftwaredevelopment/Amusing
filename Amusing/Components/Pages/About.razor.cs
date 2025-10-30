01cd2a54b287d9364e7ed99ce462dbb2e6f8c68e
herbie68 <herbert.nijkamp@gmail.com>
2025-10-30 16:59:30 +0100

    Dit some nameing refactoring.
    Add festival and group listing pages with export
    
    Introduce new Razor components `ListFestivals` and `ListGroups` with corresponding code-behind files.
    
    - `ListFestivals.razor`: Define a new page with a grid to display festival data, supporting Excel, CSV, and PDF export, multi-sorting, and text wrapping. Includes a toolbar with export buttons and a search input.
    - `ListFestivals.razor.cs`: Implement logic for component initialization, search handling, row count updates, and data export using services for logging and data retrieval.
    
    - `ListGroups.razor`: Define a new page with a grid to show group participation per festival, with filtering and year selection via a slider. Supports data export and includes a search input in the toolbar.
    - `ListGroups.razor.cs`: Manage component state, data loading, filter changes, and export functionality. Update visible row count and handle year selection.
    
    Utilize Syncfusion Blazor components and dependency injection for enhanced UI and service access.

---
+++ Amusing/Components/Pages/About.razor.cs
@@ -0,0 +1,29 @@
+using Amusing.Models;
+using Amusing.Services;
+
+using Syncfusion.Blazor.Grids;
+using Syncfusion.Blazor.Inputs;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Amusing.Components.Pages;
+
+public partial class About : ComponentBase
+{
+    [Inject] protected LoggingService LoggingService { get; set; } = default!;
+    [Inject] protected GitHubService GitHubService { get; set; } = default!;
+
+
+    protected List<CommitModel> commits = new();
+    protected string? assemblyVersion = string.Empty;
+    protected string? imageSource;
+
+    protected override async Task OnInitializedAsync()
+    {
+        imageSource = $"images/logo.svg";
+        var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+        assemblyVersion = assembly.GetName().Version?.ToString() ?? "Onbekend";
+        commits = await GitHubService.GetCommitsAsync( "hnsoftwaredevelopment", "amusing" );
+    }
+}
