9ae15dd36e2442aa45eca864f46eb7c1b819822f
herbie68 <herbert.nijkamp@gmail.com>
2025-10-30 13:50:10 +0100

    Implemented loging in Persons List page.
    Refactor List-Persons to use code-behind file
    
    Moved inline C# logic from List-Persons.razor to a new code-behind file, List-Persons.razor.cs, using the @inherits directive.
    Introduced ListPersons partial class for better separation of concerns, including dependency injection for LoggingService and PersonService.
    Implemented methods for data loading, grid actions, and export functions with user action logging.
    Enhancements improve maintainability and organization of the component.
    
    closes #16

--- Amusing/Components/Pages/List-Persons.razor.cs
+++ Amusing/Components/Pages/List-Persons.razor.cs
@@ -1,1 +1,119 @@
- 
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Amusing.Helpers;
+using Amusing.Models;
+using Amusing.Services;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+using Syncfusion.Blazor.Grids;
+using Syncfusion.Blazor.Inputs;
+
+namespace Amusing.Components.Pages;
+
+public partial class ListPersons : ComponentBase
+{
+    [Inject] protected LoggingService LoggingService { get; set; } = default!;
+    [Inject] protected PersonService PersonService { get; set; } = default!;
+
+    protected bool IsLoading = false;
+    protected bool _initialLoadDone = false;
+    protected SfGrid<PersonOverviewModel> GridRef;
+    protected int VisibleRowCount = 0;
+    protected List<PersonOverviewModel> Personen = [];
+    protected string FileName = "Personen";
+
+    protected override async Task OnInitializedAsync()
+    {
+        IsLoading = true;
+        Personen = await PersonService.GetPersonOverviewAsync();
+        IsLoading = false;
+    }
+
+    // Manage direct search functionality
+    public async void OnInput( InputEventArgs args )
+    {
+        await GridRef.SearchAsync( args.Value );
+
+        await Task.Delay( 50 );
+        await UpdateVisibleRowCountAsync();
+    }
+
+    protected async Task UpdateVisibleRowCountAsync()
+    {
+        if ( GridRef is not null )
+        {
+            await GridRef.Refresh();
+            await Task.Delay( 50 );
+            var records = await GridRef.GetCurrentViewRecordsAsync();
+            await Task.Delay( 150 );
+            VisibleRowCount = records?.Count ?? 0;
+            StateHasChanged();
+        }
+    }
+
+    protected async Task OnGridDataBound()
+    {
+        if ( !_initialLoadDone )
+        {
+            _initialLoadDone = true;
+            await UpdateVisibleRowCountAsync();
+        }
+    }
+
+    protected async Task OnGridActionComplete( ActionEventArgs<PersonOverviewModel> args )
+    {
+        if ( args.RequestType == Syncfusion.Blazor.Grids.Action.Refresh ||
+        args.RequestType == Syncfusion.Blazor.Grids.Action.Filtering ||
+        args.RequestType == Syncfusion.Blazor.Grids.Action.Paging ||
+        args.RequestType == Syncfusion.Blazor.Grids.Action.Sorting )
+        {
+            await UpdateVisibleRowCountAsync();
+        }
+    }
+
+    // Export functions
+    protected async Task ExportToExcel()
+    {
+        var exportProps = new ExcelExportProperties
+        {
+            FileName = $"{FileName}-{DateTime.Now:yyyyMMdd}-{DateTime.Now:HHmm}.xlsx"
+        };
+
+        await GridRef!.ExportToExcelAsync( exportProps );
+        await GridRef!.ExportToExcelAsync( exportProps );
+
+        string _report = $"<_userName> heeft \"{exportProps.FileName}\" geexporteerd";
+        await LoggingService.WriteUserActionAsync( "Lijsten", "Personen", "success", _report );
+    }
+
+    protected async Task ExportToCsv()
+    {
+        var exportProps = new ExcelExportProperties
+        {
+            FileName = $"{FileName}-{DateTime.Now:yyyyMMdd}-{DateTime.Now:HHmm}.csv"
+        };
+
+        await GridRef!.ExportToCsvAsync( exportProps );
+
+        string _report = $"<_userName> heeft \"{exportProps.FileName}\" geexporteerd";
+        await LoggingService.WriteUserActionAsync( "Lijsten", "Personen", "success", _report );
+    }
+
+    protected async Task ExportToPdf()
+    {
+        var exportProps = new PdfExportProperties
+        {
+            FileName = $"{FileName}-{DateTime.Now:yyyyMMdd}-{DateTime.Now:HHmm}.pdf",
+            PageOrientation = PageOrientation.Landscape,
+            PageSize=PdfPageSize.A4,
+            AllowHorizontalOverflow = true
+        };
+        await GridRef!.ExportToPdfAsync( exportProps );
+
+        string _report = $"<_userName> heeft \"{exportProps.FileName}\" geexporteerd";
+        await LoggingService.WriteUserActionAsync( "Lijsten", "Personen", "success", _report );
+    }
+} 
