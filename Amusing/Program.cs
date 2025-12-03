8e39a7908476a1f7a96ca0516614dd55fa530a55
herbie68 <herbert.nijkamp@gmail.com>
2025-12-03 11:46:59 +0100

    Adding github action for deployment

--- Amusing/Program.cs
+++ Amusing/Program.cs
@@ -54,14 +54,14 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 
 // Dump na laden
-Debug.WriteLine( "=== CONFIG DUMP ===" );
-Debug.WriteLine( "ENV: " + builder.Environment.EnvironmentName );
-Debug.WriteLine( "ConnectionString: " + builder.Configuration.GetConnectionString( "DefaultConnection" ) );
-Debug.WriteLine( "====================" );
+Console.WriteLine( "=== CONFIG DUMP ===" );
+Console.WriteLine( "ENV: " + builder.Environment.EnvironmentName );
+Console.WriteLine( "ConnectionString: " + builder.Configuration.GetConnectionString( "DefaultConnection" ) );
+Console.WriteLine( "====================" );
 
 // Debug output
-Debug.WriteLine( $"Environment: {builder.Environment.EnvironmentName}" );
-Debug.WriteLine( $"DB Connection: {( string.IsNullOrWhiteSpace( connectionString ) ? "NOT FOUND" : "FOUND" )}" );
+Console.WriteLine( $"Environment: {builder.Environment.EnvironmentName}" );
+Console.WriteLine( $"DB Connection: {( string.IsNullOrWhiteSpace( connectionString ) ? "NOT FOUND" : "FOUND" )}" );
 
 // ---------------------------------------------------------
 // 2. Email settings configuration
